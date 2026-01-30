29da929dda357b344f0f0fd20c3f94b10a4b9662
PARAG <paragdakhane123@gmail.com>
2026-01-30 11:52:06 +0530

    Intial Project setup and Finance dataset setup done

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/",(req,res) =>{
+  res.json({message:"Backend is running!!"});
+});
+
+app.listen(PORT , ()=>{
+  console.log(`Server is running at PORT ${3000}`);
+});
